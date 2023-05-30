a017ae615448401ac42c32e53486810b08b6f1ca
msaqib11 <saqib.maverick@gmail.com>
2023-05-30 14:42:55 +0500

    Delete Api with comments Added

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -1,44 +1,74 @@
-import {sql} from "@vercel/postgres"
-import { NextRequest,NextResponse } from "next/server";
-import {db,allTodo,todoTable,addTodo} from "@/lib/drizzle"
-export async function GET(request: NextRequest){
-    try {
-        await sql`
+import { sql } from "@vercel/postgres";
+import { NextRequest, NextResponse } from "next/server";
+import { db, allTodo, todoTable, addTodo } from "@/lib/drizzle";
+import { eq } from "drizzle-orm";
+export async function GET(request: NextRequest) {
+  try {
+    await sql`
         CREATE TABLE IF NOT EXISTS tasks (
           id SERIAL PRIMARY KEY,
           title VARCHAR(255) NOT NULL,
           status BOOLEAN NOT NULL DEFAULT true
         )
-      `
-     const allTasks = await db.select().from(todoTable);
-    //  console.log(allTasks.find((item)=> item.id===1));
-     console.log(allTasks);
-     
-     return NextResponse.json({allTodos:allTasks})
-    } catch (error) {
-        console.log((error as {message:string}).message);
-        return NextResponse.json({message:"Something went wrong"})
-    }
+      `; // Creating a table named 'tasks' with columns 'id', 'title', and 'status' if it does not exist already
+
+    const allTasks = await db.select().from(todoTable); // Retrieving all tasks from the 'todoTable' using the 'select' method
+
+    // console.log(allTasks.find((item)=> item.id===1));
+    console.log(allTasks); // Logging all the retrieved tasks to the console
+
+    return NextResponse.json({ allTodos: allTasks }); // Returning a JSON response containing all the tasks
+  } catch (error) {
+    console.log((error as { message: string }).message); // Logging the error message to the console
+    return NextResponse.json({ message: "Something went wrong" }); // Returning a JSON response indicating an error occurred
+  }
 }
 
-export async function POST(request:NextRequest){
-    let req = await request.json();
-    //body for our task
-    const newTask: addTodo = {
-        task: req.title,
-        status : req.status,
-      };
-    try {
-         if(req.title){
-            //insert via drizzle orm
-             const addTask  = await db.insert(todoTable).values(newTask).returning()
-             console.log(addTask);
-            return NextResponse.json({message:"Data Added successfully"})
-        }else
-            throw new Error("Task field is required");
-        }
-    catch (error) {
-        console.log((error as { message: string }).message);
-        return  NextResponse.json({ message: (error as { message: string }).message });
+
+export async function POST(request: NextRequest) {
+  let req = await request.json(); // Parsing the request body as JSON and assigning it to the 'req' variable
+
+  const newTask: addTodo = {
+    task: req.title, // Assigning the value of 'title' property from 'req' to 'task' property of 'newTask'
+    status: req.status, // Assigning the value of 'status' property from 'req' to 'status' property of 'newTask'
+  };
+
+  try {
+    if (req.title) { // Checking if 'title' property exists in 'req'
+      const addTask = await db.insert(todoTable).values(newTask).returning(); // Performing the database insertion operation using the 'newTask' object
+      console.log(addTask); // Logging the result of the insertion operation to the console
+      return NextResponse.json({ message: "Data Added successfully" }); // Returning a JSON response indicating successful data addition
+    } else {
+      throw new Error("Task field is required"); // Throwing an error if 'title' property is not provided in 'req'
     }
+  } catch (error) {
+    console.log((error as { message: string }).message); // Logging the error message to the console
+    return NextResponse.json({ message: (error as { message: string }).message }); // Returning a JSON response with the error message
+  }
+}
+
+export async function DELETE(request: NextRequest) {
+  const { url } = request; // Extracting the 'url' property from the 'request' object using destructuring
+  const parsedUrl = new URL(url); // Parsing the 'url' using the 'URL' constructor to obtain a URL object
+  const searchParams = parsedUrl.searchParams; // Accessing the 'searchParams' property of the parsed URL object to retrieve query parameters
+  const taskId = searchParams.get("taskId"); // Extracting the value of the "taskId" query parameter from the search params
+  
+  
+  try {
+    if (taskId) {
+      const deleteTask = await db
+        .delete(todoTable) // Deleting from the todoTable
+        .where(eq(todoTable.id, parseInt(taskId))) // Setting the condition for deletion based on taskId
+        .returning(); // Returning the deleted task (optional)
+        
+      return NextResponse.json({ message: "Task deleted" }); // Returning a JSON response indicating successful deletion
+    } else {
+      throw new Error("Task ID is required"); // Throwing an error if taskId is not provided
+    }
+  } catch (error) {
+    console.log((error as { message: string }).message); // Logging the error message
+    return NextResponse.json({
+      message: (error as { message: string }).message, // Returning a JSON response with the error message
+    });
+  }
 }
