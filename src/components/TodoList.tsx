92ae36d1d4cdcea8f9c2eedc8002cad7061b1ced
msaqib11 <saqib.maverick@gmail.com>
2023-06-06 03:49:09 +0500

    Key props resolved

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -33,7 +33,7 @@ const TodoList = async () => {
       {Todos.allTodos.map((item)=>(
       <div className="px-4 py-4 bg-slate-100  rounded-lg flex items-center justify-between gap-3 m-3">
         {/* <div className="w-3 h-3 bg-secondary rounded-full"></div> */}
-        <p className="text-base leading-4 ">{item.task}</p>
+        <p className="text-base leading-4" key={item.id}>{item.task}</p>
         <div className="flex gap-2">
           <AiOutlineCheck className="w-5 h-5 font-bold text-green-800 hover:text-green-300 cursor-pointer" />
           <AiFillDelete className="w-5 h-5 font-bold text-red-800 hover:text-red-500 cursor-pointer" />
